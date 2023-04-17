2b4bddcb10dfa1f0e2fd415d79514df6d3f52127
marionsmlg <marion.schimmerling2@hotmail.fr>
2023-04-17 17:50:29 +0200

    authorization

--- src/handleGET.js
+++ src/handleGET.js
@@ -26,98 +26,123 @@ export async function handleGET(request, response, requestURLData) {
   }
   const secret = "HaB>`[kP=3JNN),T";
   const authorizationApi = request.headers.authorization;
+  const apiPaths = [
+    { path: "/api/articles", dataJsonPath: ARTICLES_DATA_PATH },
+    {
+      path: "/api/articles-categories",
+      dataJsonPath: ARTICLE_CATEGORIES_DATA_PATH,
+    },
+    { path: "/api/header", dataJsonPath: HEADER_DATA_PATH },
+    { path: "/api/footer", dataJsonPath: FOOTER_DATA_PATH },
+  ];
 
-  if (requestURLData.pathname === "/api/articles") {
-    if (authorizationApi !== secret) {
-      response.status = 403;
-      response.end("Forbidden");
+  for (const api of apiPaths) {
+    if (requestURLData.pathname === api.path) {
+      if (authorizationApi !== secret) {
+        response.status = 403;
+        response.end("Forbidden");
+        return;
+      }
+      const data = await readJSON(api.dataJsonPath);
+      response.statusCode = 200;
+      response.end(JSON.stringify(data));
       return;
     }
-    const dataArticles = await readJSON(ARTICLES_DATA_PATH);
-    response.statusCode = 200;
-    response.end(JSON.stringify(dataArticles));
-    return;
-  } else if (requestURLData.pathname === "/api/articles-categories") {
-    if (authorizationApi !== secret) {
-      response.status = 403;
-      response.end("Forbidden");
-      return;
-    }
-    const dataArticlesCategories = await readJSON(ARTICLE_CATEGORIES_DATA_PATH);
-    response.statusCode = 200;
-    response.end(JSON.stringify(dataArticlesCategories));
-    return;
-  } else if (requestURLData.pathname === "/api/header") {
-    if (authorizationApi !== secret) {
-      response.status = 403;
-      response.end("Forbidden");
-      return;
-    }
-    const dataHeader = await readJSON(HEADER_DATA_PATH);
-    response.statusCode = 200;
-    response.end(JSON.stringify(dataHeader));
-    return;
-  } else if (requestURLData.pathname === "/api/footer") {
-    if (authorizationApi !== secret) {
-      response.status = 403;
-      response.end("Forbidden");
-      return;
-    }
-    const dataFooter = await readJSON(FOOTER_DATA_PATH);
-    response.statusCode = 200;
-    response.end(JSON.stringify(dataFooter));
-    return;
-  }
 
-  if (requestURLData.pathname !== `/login`) {
+    // if (requestURLData.pathname === "/api/articles") {
+    //   if (authorizationApi !== secret) {
+    //     response.status = 403;
+    //     response.end("Forbidden");
+    //     return;
+    //   }
+    //   const dataArticles = await readJSON(ARTICLES_DATA_PATH);
+    //   response.statusCode = 200;
+    //   response.end(JSON.stringify(dataArticles));
+    //   return;
+    // } else if (requestURLData.pathname === "/api/articles-categories") {
+    //   if (authorizationApi !== secret) {
+    //     response.status = 403;
+    //     response.end("Forbidden");
+    //     return;
+    //   }
+    //   const dataArticlesCategories = await readJSON(
+    //     ARTICLE_CATEGORIES_DATA_PATH
+    //   );
+    //   response.statusCode = 200;
+    //   response.end(JSON.stringify(dataArticlesCategories));
+    //   return;
+    // } else if (requestURLData.pathname === "/api/header") {
+    //   if (authorizationApi !== secret) {
+    //     response.status = 403;
+    //     response.end("Forbidden");
+    //     return;
+    //   }
+    //   const dataHeader = await readJSON(HEADER_DATA_PATH);
+    //   response.statusCode = 200;
+    //   response.end(JSON.stringify(dataHeader));
+    //   return;
+    // } else if (requestURLData.pathname === "/api/footer") {
+    //   if (authorizationApi !== secret) {
+    //     response.status = 403;
+    //     response.end("Forbidden");
+    //     return;
+    //   }
+    //   const dataFooter = await readJSON(FOOTER_DATA_PATH);
+    //   response.statusCode = 200;
+    //   response.end(JSON.stringify(dataFooter));
+    //   return;
+    // }
+
+    if (requestURLData.pathname !== `/login`) {
+      const cookieLogin = cookie.parse(request.headers.cookie || "");
+      const sessionId = cookieLogin.sessionId;
+      if (!(await hasSessionId(sessionId))) {
+        response.statusCode = 302;
+        response.setHeader("Location", `/login`);
+        response.end();
+        return;
+      }
+    }
+
+    let templatePath = `src/template${requestURLData.pathname}`;
+    const basenameURL = path.basename(requestURLData.pathname);
+
+    if (await isDir(templatePath)) {
+      templatePath = path.join(templatePath, "index.njk");
+    } else if (await isFile(`${templatePath}.njk`)) {
+      templatePath = `${templatePath}.njk`;
+    } else if (
+      (await articleIDExists(basenameURL)) &&
+      requestURLData.pathname === `/articles/${basenameURL}`
+    ) {
+      templatePath = "src/template/articles/edit.njk";
+    } else {
+      render404(response);
+      return;
+    }
+
+    const searchParams = Object.fromEntries(requestURLData.searchParams);
+    const articles = await readJSON(ARTICLES_DATA_PATH);
+    const article = articles.find((article) => article.id === basenameURL);
+    const footerData = await readJSON(FOOTER_DATA_PATH);
+    const headerData = await readJSON(HEADER_DATA_PATH);
+
     const cookieLogin = cookie.parse(request.headers.cookie || "");
     const sessionId = cookieLogin.sessionId;
-    if (!(await hasSessionId(sessionId))) {
-      response.statusCode = 302;
-      response.setHeader("Location", `/login`);
-      response.end();
-      return;
-    }
+
+    const templateData = {
+      searchParams: searchParams,
+      highlightArticles: articles.slice(0, 3),
+      articles: articles,
+      article: article,
+      articleCategories: await readJSON(ARTICLE_CATEGORIES_DATA_PATH),
+      dataNavbar: headerData.navlinks,
+      footerLinks: footerData.footerLinks,
+      footerSocialMedia: footerData.footerSocialMedia,
+      userEmail: await getUserEmail(sessionId),
+    };
+
+    const html = nunjucks.render(templatePath, templateData);
+    response.end(html);
   }
-
-  let templatePath = `src/template${requestURLData.pathname}`;
-  const basenameURL = path.basename(requestURLData.pathname);
-
-  if (await isDir(templatePath)) {
-    templatePath = path.join(templatePath, "index.njk");
-  } else if (await isFile(`${templatePath}.njk`)) {
-    templatePath = `${templatePath}.njk`;
-  } else if (
-    (await articleIDExists(basenameURL)) &&
-    requestURLData.pathname === `/articles/${basenameURL}`
-  ) {
-    templatePath = "src/template/articles/edit.njk";
-  } else {
-    render404(response);
-    return;
-  }
-
-  const searchParams = Object.fromEntries(requestURLData.searchParams);
-  const articles = await readJSON(ARTICLES_DATA_PATH);
-  const article = articles.find((article) => article.id === basenameURL);
-  const footerData = await readJSON(FOOTER_DATA_PATH);
-  const headerData = await readJSON(HEADER_DATA_PATH);
-
-  const cookieLogin = cookie.parse(request.headers.cookie || "");
-  const sessionId = cookieLogin.sessionId;
-
-  const templateData = {
-    searchParams: searchParams,
-    highlightArticles: articles.slice(0, 3),
-    articles: articles,
-    article: article,
-    articleCategories: await readJSON(ARTICLE_CATEGORIES_DATA_PATH),
-    dataNavbar: headerData.navlinks,
-    footerLinks: footerData.footerLinks,
-    footerSocialMedia: footerData.footerSocialMedia,
-    userEmail: await getUserEmail(sessionId),
-  };
-
-  const html = nunjucks.render(templatePath, templateData);
-  response.end(html);
 }
