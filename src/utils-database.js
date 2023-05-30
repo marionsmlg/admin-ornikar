bb7b9911ad3264c4833167cf8e6dee59ba7586a0
marionsmlg <marion.schimmerling2@hotmail.fr>
2023-05-30 14:04:23 +0200

    database

--- src/utils-database.js
+++ src/utils-database.js
@@ -8,6 +8,7 @@ const db = knex({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
   },
   pool: { min: 0, max: 20 },
 });
